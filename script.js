35a68f816ce7bb678eda6d67611fd813ea059de4
Toriola Faidat <faaedah@gmail.com>
2022-05-03 10:11:26 +0100

    added preloader

--- script.js
+++ script.js
@@ -39,3 +39,13 @@ gsap.to('.shape2', { x: 100, rotation: 360, duration: 10, repeat: -1, ease: 'bou
 gsap.to('.shape3', { y: 100, rotation: 360, duration: 7, repeat: -1, ease: 'bounce-in' })
 gsap.to('.shape4', { x: 40, rotation: 360, duration: 20, repeat: -1, ease: 'bounce-in' })
 gsap.to('.shape5', { x: 40, rotation: 360, duration: 20, repeat: -1, ease: 'bounce-in' })
+
+
+//remove loader spin after 3s
+setTimeout(removeLoader, 4000)
+
+function removeLoader() {
+ const loader = document.querySelector('.preloader');
+  loader.style.display = 'none';
+  //loader.style.animation = "example 2s ease"
+}
